244a546a49d86a0e4b6a8c6a2e8aa796cbf28e7a
Kamran Asim <ka2099@bath.ac.uk>
2025-10-06 13:54:42 +0400

    changes

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # LLM-HF-MOM
 This repository describes sample implementation to convert audio document to Minute of meeting (MOM) using LLama and whisper 
+
+# REQUIREMENTS
+
+1. Add HF_TOKEN in Environment Variable
+2. Install appropriate CUDA and cuDNN for GPU support
+3. Install Pytorch version compitable with CUDA
+
--- app/core/app_configuration.py
+++ app/core/app_configuration.py
@@ -3,11 +3,12 @@ import torch
 
 LLM_MODEL_PATH = "assets/models"
 LLAMA = "meta-llama/Meta-Llama-3.1-8B-Instruct"
+LLAMA_1B="meta-llama/Llama-3.2-1B-Instruct"
 AUDIO_MODEL = "whisper-1"
 
 QUANT_CONFIG = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
-    bnb_4bit_compute_dtype=torch.bfloat16,
+    bnb_4bit_compute_dtype=torch.float16,
     bnb_4bit_quant_type="nf4"
 )
